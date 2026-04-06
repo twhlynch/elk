72401cfd5fa706e4c8ea6219a4992d98b98c5ba2
darcy <acednes@gmail.com>
2026-04-06 19:49:23 +1000

    feat(cli): ensure positional arguments are given

--- bin/Cli.zig
+++ bin/Cli.zig
@@ -170,12 +170,19 @@ const templates = struct {
 
     pub fn Args(comptime template: anytype) type {
         return struct {
-            positional: ArgStruct(template.positional) = .{},
-            named: ArgStruct(template.named) = .{},
+            positional: PositionalArgs(template.positional) = .{},
+            named: NamedArgs(template.named) = .{},
         };
     }
 
-    pub fn ArgStruct(comptime template: anytype) type {
+    pub fn PositionalArgs(comptime template: anytype) type {
+        return ArgStruct(template, false);
+    }
+    pub fn NamedArgs(comptime template: anytype) type {
+        return ArgStruct(template, true);
+    }
+
+    fn ArgStruct(comptime template: anytype, comptime optional_fields: bool) type {
         const fields = @typeInfo(@TypeOf(template)).@"struct".fields;
 
         var info: struct {
@@ -185,13 +192,13 @@ const templates = struct {
         } = undefined;
 
         for (fields, 0..) |field, i| {
-            const value_type = @field(template, field.name).value;
+            const ValueRaw = @field(template, field.name).value;
+            const Value = if (optional_fields) ?ValueRaw else ValueRaw;
+            const default: Value = if (optional_fields) null else undefined;
 
             info.names[i] = field.name;
-            info.types[i] = ?value_type;
-            info.attrs[i] = .{
-                .default_value_ptr = &@as(?value_type, null),
-            };
+            info.types[i] = Value;
+            info.attrs[i] = .{ .default_value_ptr = &default };
         }
 
         return @Struct(.auto, null, &info.names, &info.types, &info.attrs);
@@ -201,27 +208,31 @@ const templates = struct {
         // TODO: Validate cli template types
 
         var args: Args(template) = .{};
+        var positional_count: usize = 0;
 
         _ = iter.next();
         while (iter.next()) |string| {
             if (try Flag.parse(string)) |flag| {
                 try addNamedArg(template.named, &args.named, flag, iter);
             } else {
-                try addPositionalArg(&args.positional, string);
+                try addPositionalArg(&args.positional, &positional_count, string);
             }
         }
+
+        if (positional_count < @typeInfo(@TypeOf(args.positional)).@"struct".fields.len)
+            return error.ExpectedPositionalArg;
 
         try checkDependencies(template.named, &args.named);
 
         return args;
     }
 
-    fn addPositionalArg(args: anytype, string: []const u8) !void {
-        inline for (@typeInfo(@TypeOf(args.*)).@"struct".fields) |field| {
-            if (@field(args, field.name) == null) {
-                const Value = @typeInfo(field.type).optional.child;
-                const value = try parseValue(Value, string);
+    fn addPositionalArg(args: anytype, positional_count: *usize, string: []const u8) !void {
+        inline for (@typeInfo(@TypeOf(args.*)).@"struct".fields, 0..) |field, i| {
+            if (i == positional_count.*) {
+                const value = try parseValue(field.type, string);
                 @field(args, field.name) = value;
+                positional_count.* += 1;
                 return;
             }
         }
@@ -230,7 +241,7 @@ const templates = struct {
 
     fn addNamedArg(
         comptime template: anytype,
-        args: *ArgStruct(template),
+        args: *NamedArgs(template),
         flag: Flag,
         iter: *ArgIterator,
     ) !void {
@@ -249,7 +260,7 @@ const templates = struct {
         return error.InvalidFlag;
     }
 
-    fn checkDependencies(comptime template: anytype, args: *const ArgStruct(template)) !void {
+    fn checkDependencies(comptime template: anytype, args: *const NamedArgs(template)) !void {
         inline for (@typeInfo(@TypeOf(template)).@"struct".fields) |field| {
             const listing: NamedListing = @field(template, field.name);
 
@@ -265,7 +276,7 @@ const templates = struct {
     fn hasAnyDependency(
         comptime template: anytype,
         comptime dependencies: []const NamedListing.Id,
-        args: *const ArgStruct(template),
+        args: *const NamedArgs(template),
     ) bool {
         inline for (dependencies) |dependency| {
             if (hasDependency(template, dependency, args))
@@ -277,7 +288,7 @@ const templates = struct {
     fn hasDependency(
         comptime template: anytype,
         dependency: NamedListing.Id,
-        args: *const ArgStruct(template),
+        args: *const NamedArgs(template),
     ) bool {
         inline for (@typeInfo(@TypeOf(template)).@"struct".fields) |field| {
             if (std.mem.eql(u8, field.name, @tagName(dependency)))
